1584a68a239b70a0db3a6a6d34f3e7a7777b4600
이승연 <dltmddus9082@naver.com>
2023-08-02 15:48:12 +0900

    add 회원가입 - id 중복체크 기능 구현

--- src/main/frontend/src/pages/Signup.js
+++ src/main/frontend/src/pages/Signup.js
@@ -24,6 +24,8 @@ const Signup = () => {
 
   });
 
+  const [isIdDuplicate, setIsIdDuplicate] = useState(true); // 중복 여부 상태 변수
+
   const onChange = (e) => {
       setState({
         ...state,
@@ -31,76 +33,93 @@ const Signup = () => {
       });
     };
 
-    const handleSubmit = async (e) => {
-      e.preventDefault(); // 폼 기본 제출 이벤트 방지
 
-      if (state.id.length < 3 ||
-          state.password.length < 5 ||
-          state.nickname.length < 5 ||
-          state.phone.length < 7 ||
-          state.email.length < 5
-          ) {
-        // 입력 값 유효성 검사
-        alert("입력값을 확인해주세요.");
-        return;
-      }
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // 폼 기본 제출 이벤트 방지
 
+    console.log("handleSubmit 함수 호출");
+    console.log("isIdDuplicate 상태:", isIdDuplicate);
 
-  try {
-        const csrfToken = getCsrfTokenFromCookie();
-
-        const response = await axios.post("http://localhost:8080/signup", null, {
-          params: {
-            id: state.id,
-            password: state.password,
-            name: state.name,
-
-            nickname: state.nickname,
-            phone: state.phone,
-            email: state.email,
-          },
-          headers: {
-            "Content-Type": "application/json",
-            "X-CSRF-TOKEN": csrfToken,
-          },
-        });
-
-        if (response.status === 200) {
-          console.log("회원가입 성공");
-          // 다음 작업을 수행하거나 페이지를 이동하면 됩니다.
-        } else {
-          console.error("회원가입 실패");
-        }
-      } catch (error) {
-        console.error("Error:", error);
-      }
-    };
-
-const handleCheckDuplicateID = async () => {
-  // 입력한 아이디 가져오기
-  const id = inputID.current.value;
-
-  // 입력 값이 없는 경우 요청을 보내지 않음
-  if (!id) {
-    alert("아이디를 입력해주세요.");
-    return;
-  }
-
-  try {
-    // 서버에 아이디 중복 검증 요청 보내기
-    const response = await axios.get("http://localhost:8080/checkDuplicateID", {
-          params: { id: id }, // 쿼리 매개변수로 아이디를 전달
-        });
-    // 서버로부터 받은 응답 확인
-    if (response.data.duplicate) {
-      alert("이미 존재하는 아이디입니다.");
-    } else {
-      alert("사용 가능한 아이디입니다.");
+    if (
+      state.id.length < 3 ||
+      state.password.length < 5 ||
+      state.nickname.length < 5 ||
+      state.phone.length < 7 ||
+      state.email.length < 5
+    ) {
+      // 입력 값 유효성 검사
+      alert("입력값을 확인해주세요.");
+      return;
     }
-  } catch (error) {
-    console.error("Error:", error);
-  }
-};
+
+    // 중복 확인을 안 한 경우, 가입 처리하지 않음
+    if (isIdDuplicate) {
+      alert("중복 확인을 해주세요.");
+      return;
+    }
+
+    try {
+      const csrfToken = getCsrfTokenFromCookie();
+
+      const response = await axios.post("http://localhost:8080/signup", null, {
+        params: {
+          id: state.id,
+          password: state.password,
+          name: state.name,
+          nickname: state.nickname,
+          phone: state.phone,
+          email: state.email,
+        },
+        headers: {
+          "Content-Type": "application/json",
+          "X-CSRF-TOKEN": csrfToken,
+        },
+      });
+
+      if (response.status === 200) {
+        console.log("회원가입 성공");
+        window.location.href = "/";
+      } else {
+        console.error("회원가입 실패");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleCheckDuplicateID = async () => {
+    // 입력한 아이디 가져오기
+    const id = state.id; // state에서 아이디 값을 가져오기
+
+    // 입력 값이 없는 경우 요청을 보내지 않음
+    if (!id) {
+      alert("아이디를 입력해주세요.");
+      return;
+    }
+
+    try {
+      // 서버에 아이디 중복 검증 요청 보내기
+      const response = await axios.get("http://localhost:8080/checkDuplicateID", {
+        params: { id: id }, // 쿼리 매개변수로 아이디를 전달
+      });
+
+    // 서버로부터 받은 응답 확인
+      const isDuplicate = response.data.duplicate;
+
+      console.log("handleCheckDuplicateID 함수 호출");
+      console.log("isDuplicate 상태:", isDuplicate);
+
+      setIsIdDuplicate(isDuplicate);
+
+      if (isDuplicate) {
+        alert("이미 존재하는 아이디입니다.");
+      } else {
+        alert("사용 가능한 아이디입니다.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
   return (
       <div>
--- src/main/java/com/web/stard/controller/MemberController.java
+++ src/main/java/com/web/stard/controller/MemberController.java
@@ -28,31 +28,41 @@ public class MemberController {
         return "sign_up";
     }
 
-    // 회원 정보를 데이터베이스에 저장
+    // 회원 가입
     @PostMapping("/signup")
     public String saveMember(@ModelAttribute Member member) {
         // 패스워드 인코딩
         String encodedPassword = passwordEncoder.encode(member.getPassword());
         member.setPassword(encodedPassword);
 
+/*
         System.out.println("이름: " + member.getName());
         System.out.println("아이디: " + member.getId());
         System.out.println("닉네임: " + member.getNickname());
         System.out.println("비밀번호: " + member.getPassword());
         System.out.println("이메일: " + member.getEmail());
         System.out.println("전화번호: " + member.getPhone());
+*/
+
+        // 중복 아이디 검증 - server
+        boolean isDuplicate = memberService.checkDuplicateMember(member.getId());
+        if (isDuplicate) {
+            System.out.println("이미 존재하는 아이디입니다.");
+            return "redirect:/signup";
+        }
 
         memberService.saveMember(member);
         return "redirect:/";
     }
 
+    // 중복 아이디 검증 - client
     @GetMapping("/checkDuplicateID")
     public ResponseEntity<Map<String, Boolean>> checkDuplicateID(@RequestParam String id) {
         Map<String, Boolean> response = new HashMap<>();
         boolean isDuplicate = memberService.checkDuplicateMember(id);
 
         System.out.println("검증 아이디: " + id);
-        System.out.println("검증 결과: " + isDuplicate);
+        System.out.println("중복 여부: " + isDuplicate);
 
         response.put("duplicate", isDuplicate);
         return ResponseEntity.ok(response);
