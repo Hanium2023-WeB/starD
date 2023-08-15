0d308fc0215727695f54758b752d62d5e9cc9d62
이승연 <dltmddus9082@naver.com>
2023-08-16 02:38:07 +0900

    ADD faq(AdminPost)

--- src/main/java/com/web/stard/service/FaqService.java
+++ src/main/java/com/web/stard/service/FaqService.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,47 +21,46 @@ import java.util.Optional;
 @Getter
 @Setter
 @Service
-public class NoticeService {
-
+public class FaqService {
     @Autowired
     MemberService memberService;
     @Autowired
     PostRepository postRepository;
 
-    // Notice 등록
-    public Post createNotice(Post post, Authentication authentication) {
+    // faq 등록
+    public Post createFaq(Post post, Authentication authentication) {
         String userId = authentication.getName();
         Member member = memberService.find(userId);
 
         post.setMember(member);
-        post.setType(PostType.NOTICE);
+        post.setType(PostType.FAQ);
 
         return postRepository.save(post);
     }
 
-    // Notice 리스트 조회 (회원, 비회원도 리스트 조회 가능)
-    public List<Post> getAllNotice(int page) {
+    // faq 리스트 조회
+    public List<Post> getAllFaq(int page) {
 
         Sort sort = Sort.by(new Sort.Order(Sort.Direction.DESC, "createdAt"));
         Pageable pageable = PageRequest.of(page-1, 10, sort);
         // page -> 배열 인덱스처럼 들어가서 -1 해야 함
         // 한 페이지에 Post 10개 (개수는 추후 수정)
-        return postRepository.findByType(PostType.NOTICE, pageable);
+        return postRepository.findByType(PostType.FAQ, pageable);
     }
 
-    // Notice 상세 조회 (회원도 상세 조회 가능)
-    public Post getNoticeDetail(Long id) {
-        Optional<Post> result = postRepository.findByIdAndType(id, PostType.NOTICE);
+    // faq 상세 조회
+    public Post getFaqDetail(Long id) {
+        Optional<Post> result = postRepository.findByIdAndType(id, PostType.FAQ);
         if (result.isPresent()) {
             return result.get();
         }
         return null;
     }
 
-    // Notice 수정
-    public Post updateNotice(Long id, Post requestPost, Authentication authentication) {
+    // faq 수정
+    public Post updateFaq(Long id, Post requestPost, Authentication authentication) {
         Member member = memberService.find(authentication.getName());
-        Post post = getNoticeDetail(id);
+        Post post = getFaqDetail(id);
 
         post.setTitle(requestPost.getTitle());
         post.setContent(requestPost.getContent());
@@ -73,8 +71,8 @@ public class NoticeService {
         return post;
     }
 
-    // Notice 삭제
-    public void deleteNotice(Long postId, Authentication authentication) {
+    // faq 삭제
+    public void deleteFaq(Long postId, Authentication authentication) {
         String userId = authentication.getName();
 
         Optional<Post> optionalPost = postRepository.findById(postId);
@@ -87,6 +85,4 @@ public class NoticeService {
             }
         });
     }
-
-
 }
