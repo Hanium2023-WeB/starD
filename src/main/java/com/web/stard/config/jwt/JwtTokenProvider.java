a80f768b5cace30c383da40146341a21813ce57e
dev_me <gjalsdud1030@naver.com>
2023-09-02 17:16:42 +0900

    edit schedule

--- src/main/java/com/web/stard/config/jwt/JwtTokenProvider.java
+++ src/main/java/com/web/stard/config/jwt/JwtTokenProvider.java
@@ -28,7 +28,7 @@ public class JwtTokenProvider {
     private static final String BEARER_TYPE = "Bearer";
     private static final long ACCESS_TOKEN_EXPIRE_TIME = 30 * 60 * 1000L;              // 30분
 
-//    private static final long ACCESS_TOKEN_EXPIRE_TIME = 60 * 1000L;              // 유효 기간 Test 용 (1분)
+    //    private static final long ACCESS_TOKEN_EXPIRE_TIME = 60 * 1000L;              // 유효 기간 Test 용 (1분)
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 7 * 24 * 60 * 60 * 1000L;    // 7일
 
     private final Key key;
