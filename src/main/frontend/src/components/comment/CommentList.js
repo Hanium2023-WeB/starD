dc4ef2a3c199f379ee91f203ad3309493dd3f746
이승연 <dltmddus9082@naver.com>
2023-09-15 02:07:04 +0900

    ADD 댓글 수정 시 수정 날짜, 시간 표시

--- src/main/frontend/src/components/comment/CommentList.js
+++ src/main/frontend/src/components/comment/CommentList.js
@@ -1,5 +1,16 @@
 import React from "react";
 
+const formatDatetime = (datetime) => {
+  const date = new Date(datetime);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const formattedDatetime = `${year}-${month}-${day} ${hours}:${minutes}`;
+  return formattedDatetime;
+};
+
 const CommentList = ({ comments, onEditClick, onRemoveClick, onReplySubmit }) => {
 console.log("^comments: ",comments);
  // 댓글 목록이 비어있을 때 빈 배열로 대체
@@ -18,7 +29,13 @@ console.log("^comments: ",comments);
               <span className="comment_remove_btn" onClick={() => onRemoveClick(comment.id)}>삭제</span>
             </div>
             <p>{comment.content}</p>
-            <span>{comment.createdAt}</span>
+            <span>{formatDatetime(comment.createdAt)}</span>
+            {comment.createdAt !== comment.updatedAt && ( // createdAt과 updatedAt이 다른 경우에만 표시
+              <>
+                <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                <span>( 수정: {formatDatetime(comment.updatedAt)} )</span>
+              </>
+            )}
             <span>&nbsp;&nbsp; | &nbsp;&nbsp;</span>
             <span className="comment_report_btn">신고</span><br/>
           </li>
