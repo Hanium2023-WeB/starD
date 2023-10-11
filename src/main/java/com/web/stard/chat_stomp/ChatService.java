82de69e02a094198b0b642ea1ccf570faa448215
이승연 <dltmddus9082@naver.com>
2023-10-12 03:22:20 +0900

    ADD 스터디 개설 시 studyId로 팀 채팅방 생성

---
+++ src/main/java/com/web/stard/chat_stomp/ChatService.java
@@ -0,0 +1,35 @@
+package com.web.stard.chat_stomp;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.*;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ChatService {
+
+    private final ChatRoomRepository chatRoomRepository;
+    private Map<String, ChatRoom> chatRooms;
+
+    @PostConstruct
+    //의존관게 주입완료되면 실행되는 코드
+    private void init() {
+        chatRooms = new LinkedHashMap<>();
+    }
+
+    //채팅방 하나 불러오기
+    public ChatRoom findById(String roomId) {
+        return chatRooms.get(roomId);
+    }
+
+    //채팅방 생성
+    public ChatRoom createRoom(long studyId) {
+        ChatRoom chatRoom = new ChatRoom(studyId);
+        chatRoomRepository.save(chatRoom);
+        return chatRoom;
+    }
+}
