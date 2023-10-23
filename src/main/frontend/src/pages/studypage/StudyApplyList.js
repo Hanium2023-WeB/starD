6180b35db0bd3ce0ac6513b06ab18ac84e01fdea
강지륜 <kjr2699@naver.com>
2023-10-24 01:56:49 +0900

    fix 지원자 거절 파라미터

--- src/main/frontend/src/pages/studypage/StudyApplyList.js
+++ src/main/frontend/src/pages/studypage/StudyApplyList.js
@@ -142,7 +142,7 @@ const StudyApplyList = () => {
             axios.put(`http://localhost:8080/api/v2/studies/${id}/select`, {}, {
                 params: {
                     applicantId: memberId,
-                    isSelect: true
+                    isSelect: false
                 },
                 withCredentials: true,
                 headers: {
