8937c355af8bc7882de4a34a76f01ce607a5f596
ckdmstn <eunsu321@naver.com>
2023-10-30 23:32:55 +0900

    add 팀원평가 로딩

--- src/main/frontend/src/pages/mypage/MemberEvaluate.js
+++ src/main/frontend/src/pages/mypage/MemberEvaluate.js
@@ -5,11 +5,14 @@ import Backarrow from "../../components/repeat_etc/Backarrow";
 import { useLocation } from "react-router-dom";
 import MemberEvaluateInsert from "../../components/evaluate/MemberEvaluateInsert";
 import "../../css/study_css/MyParticipateStudy.css";
+import Loading from "../../components/repeat_etc/Loading";
 
 import axios from "axios";
 
 const MemberEvaluate = () => {
     const accessToken = localStorage.getItem('accessToken');
+
+    const [loading, setLoading] = useState(true);
 
     const [evaluation, setEvaluation] = useState([]);
     const [showEvaluateInsert, setShowEvaluateInsert] = useState(false);
@@ -26,6 +29,8 @@ const MemberEvaluate = () => {
 
 
     useEffect(() => {
+        setLoading(true);
+
         axios.get(`http://localhost:8080/api/v2/studies/${studyId}/study-member`, {
             withCredentials: true,
             headers: {
@@ -47,6 +52,8 @@ const MemberEvaluate = () => {
     }, [accessToken]);
 
     useEffect(() => {
+        setLoading(true);
+
         axios.get(`http://localhost:8080/rate/member/${studyId}`, {
             withCredentials: true,
             headers: {
@@ -57,6 +64,8 @@ const MemberEvaluate = () => {
                 console.log("평가 내역 get 성공 : ", res.data);
 
                 setEvaluation(res.data);
+
+                setLoading(false);
             })
             .catch((error) => {
                 console.error("평가 내역 불러오기 실패 : ", error);
@@ -72,39 +81,41 @@ const MemberEvaluate = () => {
                     <p id={"entry-path"}> 스터디 참여 내역 > 팀원 평가 </p>
                     <Backarrow subname={"팀원 평가"} />
                     <div className="evaluate">
-                        {showEvaluateInsert ? ( // Render MemberEvaluateInsert when showEvaluateInsert is true
-                            <MemberEvaluateInsert studyId={studyId} members={Member}
-                                completeEvaluation={evaluation} />
-                        ) : evaluation.length === 0 ? (
-                            <>
-                                <p>평가 내역이 없습니다.<br/> 팀원 평가를 진행해주세요.</p>
-                            </>
-                        ) : (
-                            <table className="evaluate_table">
-                                <thead>
-                                <tr>
-                                    <th>팀원 이름</th>
-                                    <th>점수</th>
-                                    <th>사유</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                {evaluation.map((eva, index) => (
-                                    <tr className="evaluate_list">
-                                        <td className="member_name">{eva.target.nickname}</td>
-                                        <td className="member_rating">{eva.starRating}</td>
-                                        <td className="member_evaluate_reason">{eva.reason}</td>
+                        {loading ? <Loading/> : (
+                            showEvaluateInsert ? ( // Render MemberEvaluateInsert when showEvaluateInsert is true
+                                <MemberEvaluateInsert studyId={studyId} members={Member}
+                                    completeEvaluation={evaluation} />
+                            ) : evaluation.length === 0 ? (
+                                <>
+                                    <p>평가 내역이 없습니다.<br/> 팀원 평가를 진행해주세요.</p>
+                                </>
+                            ) : (
+                                <table className="evaluate_table">
+                                    <thead>
+                                    <tr>
+                                        <th>팀원 이름</th>
+                                        <th>점수</th>
+                                        <th>사유</th>
                                     </tr>
-                                ))}
+                                    </thead>
+                                    <tbody>
+                                    {evaluation.map((eva, index) => (
+                                        <tr className="evaluate_list">
+                                            <td className="member_name">{eva.target.nickname}</td>
+                                            <td className="member_rating">{eva.starRating}</td>
+                                            <td className="member_evaluate_reason">{eva.reason}</td>
+                                        </tr>
+                                    ))}
 
-                                {/*{members.map((member) => (*/}
-                                {/*    <tr className="evaluate_list">*/}
-                                {/*        <td className="community_category">{}</td>*/}
-                                {/*        <td className="community_nickname">{}</td>*/}
-                                {/*        <td className="community_datetime">{}</td>*/}
-                                {/*    </tr>*/}
-                                </tbody>
-                            </table>
+                                    {/*{members.map((member) => (*/}
+                                    {/*    <tr className="evaluate_list">*/}
+                                    {/*        <td className="community_category">{}</td>*/}
+                                    {/*        <td className="community_nickname">{}</td>*/}
+                                    {/*        <td className="community_datetime">{}</td>*/}
+                                    {/*    </tr>*/}
+                                    </tbody>
+                                </table>
+                            )
                         )}
 
                         {evaluation.length < Member.length - 1 && !showEvaluateInsert && (
