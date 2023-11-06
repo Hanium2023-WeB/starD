16864cb795e927f17445b747645d63036c4aa396
dev_me <gjalsdud1030@naver.com>
2023-11-07 02:02:39 +0900

    fix: 홈화면 반응형 css 수정

--- src/main/frontend/src/util/check.js
+++ src/main/frontend/src/util/check.js
@@ -1,5 +1,5 @@
 export function isEmail(asValue) {
-    var regExp = /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+    var regExp = /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/;
     return regExp.test(asValue);  
 }
 export function isPassword(asValue){
